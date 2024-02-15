e0a5eb064a20e9cbee50fc9578c26f08f1263793
jenish-1990 <ajakhed123@gmail.com>
2024-02-15 02:24:56 -0500

    Input done

--- src/Battleship.java
+++ src/Battleship.java
@@ -4,6 +4,7 @@
 // Written by: Jenish Pravinbhai Akhed, 40270365
 // -----------------------------------------------------
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -13,60 +14,90 @@ public class Battleship {
 
         System.out.println("Welcome to the BATTLESHIP");
         System.out.println("--------------------------");
-        System.out.println("May Force Be With You -Star Wars");
+        System.out.println("May Force Be With You -Star Wars\n");
 
         String[] positionsOfShipsAndGrenade = new String[10];
 
         for (int i = 0; i < 6; i++) {
-            try {
-                System.out.format("Enter the coordinates of your ship #%d: ", i+1);
-                positionsOfShipsAndGrenade[i] = scanner.next();
-                if (usedCoordinates(positionsOfShipsAndGrenade, i)){
-                    throw new AlreadyInUseCoordinates();
-                } else if (outsideGridCoordinates(positionsOfShipsAndGrenade, i)) {
-                    throw new OutsideTheGridCoordinates();
+            boolean flag= true;
+            while (flag){
+                try {
+                    System.out.format("Enter the coordinates of your ship #%d: ", i+1);
+                    String valueOfShipGrenade = scanner.next().toUpperCase();
+                    if (usedCoordinates(positionsOfShipsAndGrenade, valueOfShipGrenade)){
+                        throw new AlreadyInUseCoordinates();
+                    } else if (outsideGridCoordinates(valueOfShipGrenade)) {
+                        throw new OutsideTheGridCoordinates();
+                    } else {
+                        positionsOfShipsAndGrenade[i] = valueOfShipGrenade;
+                        flag = false;
+                    }
+                } catch (AlreadyInUseCoordinates e){
+                    System.out.println(e.getMessage());
+                } catch (OutsideTheGridCoordinates e) {
+                    System.out.println(e.getMessage());
                 }
-            } catch (AlreadyInUseCoordinates | OutsideTheGridCoordinates e){
-                System.out.println(e);
             }
+        }
+
+        System.out.println("");
+
+        int count = 0;
+        for (int i = 6; i < 10; i++) {
+            count++;
+            boolean flag= true;
+            while (flag){
+                try {
+                    System.out.format("Enter the coordinates of your grenade #%d: ", count);
+                    String valueOfShipGrenade = scanner.next().toUpperCase();
+                    if (usedCoordinates(positionsOfShipsAndGrenade, valueOfShipGrenade)){
+                        throw new AlreadyInUseCoordinates();
+                    } else if (outsideGridCoordinates(valueOfShipGrenade)) {
+                        throw new OutsideTheGridCoordinates();
+                    } else {
+                        positionsOfShipsAndGrenade[i] = valueOfShipGrenade;
+                        flag = false;
+                    }
+                } catch (AlreadyInUseCoordinates e){
+                    System.out.println(e.getMessage());
+                } catch (OutsideTheGridCoordinates e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        for (String s: positionsOfShipsAndGrenade){
+            System.out.println(s);
         }
     }
 
-    public static boolean usedCoordinates(String[] positionsOfShipsAndGrenade, int i) {
+    public static boolean usedCoordinates(String[] positionsOfShipsAndGrenade, String valueOfShip) {
         for (String s : positionsOfShipsAndGrenade) {
-            if (Objects.equals(positionsOfShipsAndGrenade[i], s)) {
+            if (Objects.equals(valueOfShip, s)) {
                 return true;
             }
         }
         return false;
     }
 
-//    public static boolean outsideGridCoordinates(String[] positionsOfShipsAndGrenade, int i) {
-//        return positionsOfShipsAndGrenade[i].toLowerCase().charAt(0) < 'a' || positionsOfShipsAndGrenade[i].toLowerCase().charAt(0) > 'h' || Integer.parseInt(String.valueOf(positionsOfShipsAndGrenade[i].charAt(1))) < 1 || Integer.parseInt(String.valueOf(positionsOfShipsAndGrenade[i].charAt(1))) > 8;
-//    }
-//}
-
-    public static boolean outsideGridCoordinates(String[] positionsOfShipsAndGrenade, int i) {
-        if (positionsOfShipsAndGrenade[i].toLowerCase().charAt(i) < 'a' || positionsOfShipsAndGrenade[i].toLowerCase().charAt(i) > 'h')
+    public static boolean outsideGridCoordinates(String valueOfShip) {
+        if (valueOfShip.charAt(0) < 'A' || valueOfShip.charAt(0) > 'H') {
             return true;
-        if (positionsOfShipsAndGrenade[i].toLowerCase().charAt(i) > 'a' || positionsOfShipsAndGrenade[i].toLowerCase().charAt(i) < 'h') {
-            if (integerAfterCharacter(positionsOfShipsAndGrenade[i]) < 1 || integerAfterCharacter(positionsOfShipsAndGrenade[i]) > 8) {
-                return true;
-            }else {
-                return false;
-            }
         }
-        return false;
+        else if (valueOfShip.charAt(0) >= 'A' || valueOfShip.charAt(0) <= 'H') {
+            return integerAfterCharacter(valueOfShip) < 1 || integerAfterCharacter(valueOfShip) > 8;
+        } else {
+            return false;
+        }
     }
 
-    public static int integerAfterCharacter(String positionsOfShipsAndGrenade){
-        String str = null;
-        for (int j = 1; j < positionsOfShipsAndGrenade.length(); j++) {
-            char ch = positionsOfShipsAndGrenade.charAt(j);
-            str = str + ch;
+    public static int integerAfterCharacter(String valueOfShip){
+        String str = "";
+        for (int j = 1; j < valueOfShip.length(); j++) {
+            char ch = valueOfShip.charAt(j);
+            str += ch;
         }
-        int temp = Integer.parseInt(str);
-        return temp;
+        return Integer.parseInt(str);
     }
 
 }
@@ -75,10 +106,18 @@ class AlreadyInUseCoordinates extends Exception{
     public AlreadyInUseCoordinates(){
         super("Sorry, coordinates are already in use. Please try again");
     }
+
+    public String getMessage(){
+        return super.getMessage();
+    }
 }
 
 class OutsideTheGridCoordinates extends Exception{
     public OutsideTheGridCoordinates(){
         super("Sorry, coordinates are outside the grid. Please try again");
+    }
+
+    public String getMessage(){
+        return super.getMessage();
     }
 }
